778acfb6c22c2a531da005ec404c8b550599002c
Andrew Ho <amh7705@psu.edu>
2025-09-21 01:12:30 -0400

    Complete first part of module 3 project

--- module_2/clean.py
+++ module_2/clean.py
@@ -1,38 +1,46 @@
 import json
 from .scrape import scrape_survey_page, scrape_raw_data
 
+
 # Remove fields from the dataset according to the example in the pdf
 def _remove_falsy_keys(entry):
-  return {k: v for k, v in entry.items() if k in 'comments' or v not in ('0', '0.00', '')}
+    return {
+        k: v for k, v in entry.items() if k in "comments" or v not in ("0", "0.00", "")
+    }
+
 
 def clean_data(raw_data):
-  cleaned_list = []
-  for entry in raw_data:
-    # Append university to program
-    if "program" in entry and "university" in entry and entry["university"]:
-        entry["program"] += f', {entry["university"]}'
-        entry["university"] = ""
+    cleaned_list = []
+    for entry in raw_data:
+        # Append university to program
+        if "program" in entry and "university" in entry and entry["university"]:
+            entry["program"] += f', {entry["university"]}'
+            entry["university"] = ""
 
-    cleaned = {k: v for k, v in entry.items() if v or k == "comments"}
-    if "comments" not in cleaned:
-        cleaned["comments"] = ""
-    cleaned_list.append(cleaned)
-  return [_remove_falsy_keys(entry) for entry in raw_data]
+        cleaned = {k: v for k, v in entry.items() if v or k == "comments"}
+        if "comments" not in cleaned:
+            cleaned["comments"] = ""
+        cleaned_list.append(cleaned)
+    return [_remove_falsy_keys(entry) for entry in raw_data]
+
 
 def load_data(path):
-  with open(path, "r", encoding="utf-8") as f:
-      return json.load(f)
+    with open(path, "r", encoding="utf-16") as f:
+        return json.load(f)
+
 
 def save_data(data, path):
-  with open(path, "w", encoding="utf-8") as f:
-      json.dump(data, f, indent=4)
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
 
-survey_data = scrape_survey_page(pages=1600)
-raw_data = scrape_raw_data(survey_data)
-cleaned = clean_data(raw_data)
 
-save_data(cleaned, "applicant_data.json")
+if __name__ == "__main__":
+    survey_data = scrape_survey_page(pages=1600)
+    raw_data = scrape_raw_data(survey_data)
+    cleaned = clean_data(raw_data)
 
-loaded = load_data("applicant_data.json")
+    save_data(cleaned, "applicant_data.json")
 
-print(len(loaded))
+    loaded = load_data("applicant_data.json")
+
+    print(len(loaded))
